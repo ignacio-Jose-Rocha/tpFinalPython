152773b93c88fc7485e96941296e2cd7a5992ecd
maxi <maxirobilottapons@gmail.com>
2024-05-13 20:51:51 -0300

    Creo funciòn eliminar_contacto

--- proyecto.py
+++ proyecto.py
@@ -7,3 +7,18 @@ class Contacto:
         self.email = email
         self.favorito = False
 
+
+
+    def eliminar_contacto(self):
+        self.mostrar_contactos()
+        nombre = input("Ingrese el nombre del contacto que desea buscar: ")
+        encontrado = False
+        for contacto in self.contactos:
+            if contacto.nombre.lower() == nombre.lower():
+                encontrado = True
+                self.contactos.remove(contacto)
+                print(f"El contacto {nombre} ha sido eliminado.")
+                self.eliminar_contacto_db(nombre)
+                break
+        if not encontrado:
+            print("Contacto no encontrado.")
