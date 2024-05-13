182da783746189db94f15c61aac35ac8b8d8fa9a
Enzo <enzoastorga98@gmail.com>
2024-05-13 20:10:13 -0300

    Creo archivo proyecto y clase contacto

---
+++ proyecto.py
@@ -0,0 +1,8 @@
+
+
+class Contacto:
+    def __init__(self, nombre, telefono, email):
+        self.nombre = nombre
+        self.telefono = telefono
+        self.email = email
+        self.favorito = False
